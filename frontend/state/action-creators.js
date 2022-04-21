f8285e39a3422add497b46cba88c11b3338fb989
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-21 16:44:06 -0400

    imported action types into creators, set apis to variables

--- frontend/state/action-creators.js
+++ frontend/state/action-creators.js
@@ -1,17 +1,33 @@
 // ❗ You don't need to add extra action creators to achieve MVP
-export function moveClockwise() { }
+import axios from "axios";
 
-export function moveCounterClockwise() { }
+import {
+  MOVE_CLOCKWISE,
+  MOVE_COUNTERCLOCKWISE,
+  SET_QUIZ_INTO_STATE,
+  SET_SELECTED_ANSWER,
+  SET_INFO_MESSAGE,
+  INPUT_CHANGE,
+  RESET_FORM,
+} from "./action-types";
 
-export function selectAnswer() { }
+const getQuizAPI = "http://localhost:9000/api/quiz/next";
+const postQuizAPI = "http://localhost:9000/api/quiz/new";
+const postAnswerAPI = "http://localhost:9000/api/quiz/answer";
 
-export function setMessage() { }
+export function moveClockwise() {}
 
-export function setQuiz() { }
+export function moveCounterClockwise() {}
 
-export function inputChange() { }
+export function selectAnswer() {}
 
-export function resetForm() { }
+export function setMessage() {}
+
+export function setQuiz() {}
+
+export function inputChange() {}
+
+export function resetForm() {}
 
 // ❗ Async action creators
 export function fetchQuiz() {
@@ -19,7 +35,7 @@ export function fetchQuiz() {
     // First, dispatch an action to reset the quiz state (so the "Loading next quiz..." message can display)
     // On successful GET:
     // - Dispatch an action to send the obtained quiz to its state
-  }
+  };
 }
 export function postAnswer() {
   return function (dispatch) {
@@ -27,13 +43,13 @@ export function postAnswer() {
     // - Dispatch an action to reset the selected answer state
     // - Dispatch an action to set the server message to state
     // - Dispatch the fetching of the next quiz
-  }
+  };
 }
 export function postQuiz() {
   return function (dispatch) {
     // On successful POST:
     // - Dispatch the correct message to the the appropriate state
     // - Dispatch the resetting of the form
-  }
+  };
 }
 // ❗ On promise rejections, use log statements or breakpoints, and put an appropriate error message in state
