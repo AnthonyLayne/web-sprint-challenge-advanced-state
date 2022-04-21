b6b753dc24dfc37aea2da56389308fa248b34f5b
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-21 16:49:35 -0400

    imported types into reducers file

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,33 +1,43 @@
 // ❗ You don't need to add extra reducers to achieve MVP
-import { combineReducers } from 'redux'
+import { combineReducers } from "redux";
 
-const initialWheelState = 0
+import {
+  MOVE_CLOCKWISE,
+  MOVE_COUNTERCLOCKWISE,
+  SET_QUIZ_INTO_STATE,
+  SET_SELECTED_ANSWER,
+  SET_INFO_MESSAGE,
+  INPUT_CHANGE,
+  RESET_FORM,
+} from "./action-types";
+
+const initialWheelState = 0;
 function wheel(state = initialWheelState, action) {
-  return state
+  return state;
 }
 
-const initialQuizState = null
+const initialQuizState = null;
 function quiz(state = initialQuizState, action) {
-  return state
+  return state;
 }
 
-const initialSelectedAnswerState = null
+const initialSelectedAnswerState = null;
 function selectedAnswer(state = initialSelectedAnswerState, action) {
-  return state
+  return state;
 }
 
-const initialMessageState = ''
+const initialMessageState = "";
 function infoMessage(state = initialMessageState, action) {
-  return state
+  return state;
 }
 
 const initialFormState = {
-  newQuestion: '',
-  newTrueAnswer: '',
-  newFalseAnswer: '',
-}
+  newQuestion: "",
+  newTrueAnswer: "",
+  newFalseAnswer: "",
+};
 function form(state = initialFormState, action) {
-  return state
+  return state;
 }
 
-export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form })
+export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form });
